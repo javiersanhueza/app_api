ac897da1309fbe5d523d8f8047849e1848c44c9b
Javier  Sanhueza <javier.sanhueza.14@sansano.usm.cl>
2023-07-11 12:12:48 -0400

    login user

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
   before_action :ensure_params_exist, only: :create
 
+  # Sign up
   def create
     user = User.new(user_params)
 
     if user.save
-      json_response "Signed Up Succesfully", true, { user: user }, :ok
+      json_response('Signed Up Succesfully', true, { user: user }, :ok)
     else
-      json_response "Something wrong", false, {}, :unprocessable_entity
+      json_response('Something wrong', false, { errors: user.errors }, :unprocessable_entity)
     end
   end
 
@@ -19,6 +20,7 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 
   def ensure_params_exist
     return if params[:user].present?
-    json_response "Missing params", false, {}, :bad_request
+
+    json_response('Missing params', false, {}, :bad_request)
   end
 end
